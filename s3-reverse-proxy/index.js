21e7ef717fc2a0ee41758173e59c42786388e6f8
ChinmayMohite <chinmaymohite39@gmail.com>
2024-07-12 23:15:25 +0530

    Reverse-Proxy for S3

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const app = express();
+
+const PORT = 8000;
+const BASE_PATH = ""
+const proxy = httpProxy.createProxy();
+
+app.use((req,res)=>{
+  const hostName = req.hostname;
+  const subdomian = hostName.split('.')[0];
+  const resolveTo = `${BASE_PATH}/${subdomian}`;
+  proxy.web(req,res,{target: resolveTo, changeOrigin: true});
+})
+
+proxy.on('proxyReq',(proxyRep,req,res)=>{
+  const url = req.url;
+  if(url === '/'){
+    proxyRep.path += 'index.html';
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
